3142f1ce54042e7c9ed2157150b49f126c7d30f1
Eitan Mosenkis <eitan@mosenkis.net>
2012-06-18 23:15:03 -0400

    Chapter 8

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,8 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -58,9 +60,7 @@ describe User do
 
     it { should_not be_valid }
   end
-
-  it { should respond_to(:authenticate) }
-  
+ 
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
@@ -110,5 +110,9 @@ describe User do
   describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
+  end
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
